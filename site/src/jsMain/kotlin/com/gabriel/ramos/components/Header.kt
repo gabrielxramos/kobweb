d471680882336327bdf13949175169bb44b940d7
gabriel.ramos <gabri.elramos@live.com>
2023-06-06 00:45:49 -0300

    implemented styles and animations in HeaderSection

--- site/src/jsMain/kotlin/com/gabriel/ramos/components/Header.kt
+++ site/src/jsMain/kotlin/com/gabriel/ramos/components/Header.kt
@@ -3,9 +3,12 @@ package com.gabriel.ramos.components
 import androidx.compose.runtime.Composable
 import com.gabriel.ramos.models.Section
 import com.gabriel.ramos.models.Theme
+import com.gabriel.ramos.styles.LogoStyle
+import com.gabriel.ramos.styles.NavigationItemStyle
 import com.gabriel.ramos.util.Constants.FONT_FAMILY
 import com.gabriel.ramos.util.Res
 import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextDecorationLine
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
@@ -13,6 +16,7 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.toModifier
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 
@@ -34,6 +38,7 @@ fun header() {
 fun leftSide() {
     Row {
         Image(
+            modifier = LogoStyle.toModifier(),
             src = Res.Image.logo,
             desc = "Logo Image",
         )
@@ -52,11 +57,12 @@ fun rightSide() {
     ) {
         Section.values().take(6).forEach { section ->
             Link(
-                modifier = Modifier
+                modifier = NavigationItemStyle.toModifier()
                     .padding(right = 30.px)
                     .fontFamily(FONT_FAMILY)
                     .fontSize(18.px)
-                    .fontWeight(FontWeight.Normal),
+                    .fontWeight(FontWeight.Normal)
+                    .textDecorationLine(TextDecorationLine.None),
                 path = section.path,
                 text = section.title
             )
